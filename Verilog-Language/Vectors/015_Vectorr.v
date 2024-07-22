ccad8dcc8a2c929717ed36944595dbfb701ed447
Dongvin Park <dongvin.park@gmail.com>
2024-07-22 22:39:40 +0900

    -

--- Verilog-Language/Vectors/015_Vectorr.v
+++
@@ -1,8 +0,0 @@
-module top_module( 
-    input wire [7:0] in,
-    output wire [7:0] out
-);
-	
-    assign out = {in[0], in[1], in[2], in[3], in[4], in[5], in[6], in[7]};
-    
-endmodule
--- Verilog-Language/Vectors/016_Vector4.v
+++
@@ -1,8 +0,0 @@
-module top_module (
-    input wire [7:0] in,
-    output wire [31:0] out 
-);
-
-    assign out = {{24{in[7]}}, {in[7:0]}};
-    
-endmodule
