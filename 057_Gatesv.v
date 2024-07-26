c911c678fb66a82afae6075f465575e03c7928ef
Dongvin Park <dongvin.park@gmail.com>
2024-07-26 13:56:46 +0900

    Update 057_Gatesv.v

--- 057_Gatesv.v
+++ 057_Gatesv.v
@@ -1,8 +1,7 @@
-```verilog
 module top_module(
     input  wire [3:0] in,
     output wire [2:0] out_both,
-   	output wire [3:1] out_any,
+	output wire [3:1] out_any,
     output wire [3:0] out_different
 );
     
